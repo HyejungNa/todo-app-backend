f33faa11d4a64092f8fd6ec288092421f2bd3f9d
Hyejung Na <hyejung.na01@gmail.com>
2024-10-24 03:30:23 +1100

    Add task author information

--- model/Task.js
+++ model/Task.js
@@ -11,6 +11,11 @@ const taskSchema = Schema(
       type: Boolean,
       required: true,
     },
+    author: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
   { timestamps: true, versionKey: false } // 스키마에서 애초에 설정으로 __v 빼주기 (.select("-__v") 사용하는대신에)
 );
--- model/User.js
+++ model/User.js
@@ -37,5 +37,5 @@ userSchema.methods.generateToken = function () {
   return token;
 };
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
